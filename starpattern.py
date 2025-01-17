5fbb88bd2472b4ae3dd041f244c370ada5d8ceeb
Ashu1234 <gidaveerashwini@gmail.com>
2025-01-17 18:48:29 +0530

    Initial commit

--- starpattern.py
+++ starpattern.py
@@ -99,3 +99,50 @@ for i in range(rows):
     print()
 
 
+row = 5
+col = 5
+for i in range(row):
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
+
+print()
+
+for i in range(row):
+    for j in range(i, row-1):
+        print("*", end=" ")
+    print()
+
+
+row = 4
+col = 5
+for i in range(row):
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
+for i in range(row):
+    for j in range(i, row-1):
+        print("*", end=" ")
+    print()
+
+print()
+for i in range(row):
+    for j in range(i+1):
+        print("*", end="")
+    for j in range(i, row-1):
+        print(' ', end="")
+    for j in range(i, row-1):
+        print(' ', end="")
+    for j in range(i+1):
+        print("*", end="")
+    print()
+for i in range(row):
+    for j in range(i,row-1):
+        print("*", end="")
+    for j in range(i+1):
+        print(' ', end="")
+    for j in range(i+1):
+        print(' ', end="")
+    for j in range(i, row-1):
+        print("*", end="")
+    print()
