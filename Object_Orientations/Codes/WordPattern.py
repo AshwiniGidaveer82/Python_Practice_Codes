2fbbc2cd32375e50667058860a1b7b5a11970f13
Ashu1234 <gidaveerashwini@gmail.com>
2025-02-01 17:49:01 +0530

    Initial commit

---
+++ Object_Orientations/Codes/WordPattern.py
@@ -0,0 +1,10 @@
+from itertools import zip_longest
+
+def wordPattern(self, pattern: str, s: str) -> bool:
+    s = s.split()
+    return (len(set(pattern)) ==
+            len(set(s)) ==
+            len(set(zip_longest(pattern,s))))
+
+
+print(wordPattern("abba", "dog cat cat dog")) 
