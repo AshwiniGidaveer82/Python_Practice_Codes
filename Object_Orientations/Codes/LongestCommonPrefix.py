e4a3203a3841fdc2f2b94c04b08cf2975d4d4798
Ashu1234 <gidaveerashwini@gmail.com>
2025-02-18 10:18:32 +0530

    Initial commit

---
+++ Object_Orientations/Codes/LongestCommonPrefix.py
@@ -0,0 +1,16 @@
+def longestCommonPrefix(strs):
+    if not strs:
+        return ""
+
+    for i in range(len(strs[0])):  
+        char = strs[0][i]
+        for string in strs[1:]: 
+            if i >= len(string) or string[i] != char:
+                return strs[0][:i]  
+
+    return strs[0]  
+
+
+print(longestCommonPrefix(["flower", "flow", "flight"]))  # Output: "fl"
+print(longestCommonPrefix(["dog", "racecar", "car"]))     # Output: ""
+print(longestCommonPrefix(["apple", "ape", "apricot"]))   # Output: "ap"
