b141f0861debbb6b47e9a5d21a699c0de9557166
Ashu1234 <gidaveerashwini@gmail.com>
2025-03-06 22:59:39 +0530

    Initial commit

---
+++ Object_Orientations/Codes/ToLowerCase.py
@@ -0,0 +1,6 @@
+def toLowerCase(s):
+    return s.lower()
+
+print(toLowerCase("Hello"))   # Output: "hello"
+print(toLowerCase("here"))    # Output: "here"
+print(toLowerCase("LOVELY"))  # Output: "lovely"
