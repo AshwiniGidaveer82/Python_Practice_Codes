73e4d4790fe8d3d9d4e60bf0acde05b1b96dc8bc
Ashu1234 <gidaveerashwini@gmail.com>
2025-03-17 10:19:12 +0530

    Initial commit

---
+++ Object_Orientations/Codes/Basics_Program/Perfect_SquareNumber.py
@@ -0,0 +1,15 @@
+import math
+
+def is_perfect_square(n):
+    if n < 0:
+        return False  
+    
+    sqrt_n = int(math.sqrt(n))
+    return sqrt_n * sqrt_n == n  
+
+
+n = int(input("Enter a number: "))
+if is_perfect_square(n):
+    print(f"{n} is a perfect square.")
+else:
+    print(f"{n} is NOT a perfect square.")
