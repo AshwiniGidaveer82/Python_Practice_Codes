4618039580270e2f1caa6cd752a7e135b83fe0f6
Ashu1234 <gidaveerashwini@gmail.com>
2025-04-03 09:32:45 +0530

    Inirial commit

---
+++ Object_Orientations/Codes/New.py
@@ -0,0 +1,8 @@
+def countFullyTypedWords(text: str, brokenLetters: str) -> int:
+    words = text.split()  
+    broken_set = set(brokenLetters) 
+    return sum(all(ch not in broken_set for ch in word) for word in words)  
+
+print(countFullyTypedWords("hello world", "ad"))  # Output: 1
+print(countFullyTypedWords("leet code", "lt"))   # Output: 1
+print(countFullyTypedWords("leet code", "e"))    # Output: 0
