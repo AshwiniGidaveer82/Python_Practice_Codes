e5ca70b7ad1349e3af81aa403d36279df75e9cbd
Ashu1234 <gidaveerashwini@gmail.com>
2025-04-23 09:30:36 +0530

    Initial commit

---
+++ Object_Orientations/Codes/SortArrToBinary.py
@@ -0,0 +1,45 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def sortedArrayToBST(self, nums):
+        if not nums:
+            return None
+
+        mid = len(nums) // 2
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid+1:])
+        return root
+
+from collections import deque
+
+def level_order(root):
+    if not root:
+        return []
+
+    result = []
+    queue = deque([root])
+
+    while queue:
+        node = queue.popleft()
+        if node:
+            result.append(node.val)
+            queue.append(node.left)
+            queue.append(node.right)
+        else:
+            result.append(None)
+
+    while result and result[-1] is None:
+        result.pop()
+
+    return result
+
+
+nums = [-10, -3, 0, 5, 9]
+sol = Solution()
+tree = sol.sortedArrayToBST(nums)
+print(level_order(tree))  
