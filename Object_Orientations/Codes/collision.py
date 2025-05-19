969c8adb2250c23284b960930f5dc102dc441157
Ashu1234 <gidaveerashwini@gmail.com>
2025-05-19 12:09:05 +0530

    Initial commit

---
+++ Object_Orientations/Codes/collision.py
@@ -0,0 +1,23 @@
+class Solution:
+    def asteroidCollision(self, asteroids):
+        stack = []
+
+        for ast in asteroids:
+            while stack and ast < 0 < stack[-1]:
+                if stack[-1] < -ast:
+                    stack.pop()
+                    continue
+                elif stack[-1] == -ast:
+                    stack.pop()
+                    break
+                else:
+                    break
+            else:
+                stack.append(ast)
+
+        return stack
+sol = Solution()
+
+print(sol.asteroidCollision([5, 10, -5]))   # Output: [5, 10]
+print(sol.asteroidCollision([8, -8]))       # Output: []
+print(sol.asteroidCollision([10, 2, -5]))   # Output: [10]
